c050223caaef383c7da6d3856349408e96f133f0
omrvrl <omrvrl7@gmail.com>
2023-10-06 17:56:07 +0300

    iterator involved

--- json_2_User_app.py
+++ json_2_User_app.py
@@ -1,40 +1,53 @@
-
 import json
 import os
 
-#DECORATOR
-def CheckPath(func):                
+# DECORATOR
+def CheckPath(func):
     def wrapper(self):
         if os.path.exists('users.json'):
             func(self)
         else:
             return False
     return wrapper
-#CLASS
+
+# CLASS
 class User:
-    def __init__(self,username,password,email):
+    def __init__(self, username, password, email):
         self.username = username
         self.password = password
         self.email = email
-#CLASS
+
+# CLASS
 class UserRepository:
     def __init__(self):
         self.IsLoggedIn = False
         self.users = []
         self.loadusers()
-        
-    def Register(self,user:User):
+        self.index = 0  # Initialize the index for custom __next__
+
+    def __iter__(self):                                               #            }
+        return self
+
+    def __next__(self):
+        if self.index < len(self.users):                             #           CLASS İÇERİSİNDE CLASS İTERATOR KULLANMA
+            current_user = self.users[self.index]
+            self.index += 1
+            return current_user
+        else:
+            raise StopIteration                                      #               }
+
+    def Register(self, user: User):
         self.users.append(user)
         self.savetofile()
-        
-    def Login(self,username,password):
-        
-        for user in self.users:
+
+    def Login(self, username, password):              
+        for user in self:                                            #             BURADA KULLANILDI 
             if user.username == username and user.password == password:
-                print('Loggin Successfull.')
-                self.IsLoggedIn == True 
-            else:
-                print('Access denied !!')
+                self.IsLoggedIn = True
+                print('Logging successful')
+                break
+        else:
+            print('Access denied !!')
 
     def Logout(self):
         self.IsLoggedIn = False
@@ -46,54 +59,53 @@ class UserRepository:
             print('You are not logged in')
 
     def savetofile(self):
-        list = []
+        user_list = []
         for user in self.users:
-            list.append(json.dumps(user.__dict__))
+            user_list.append(json.dumps(user.__dict__))
         
-        with open('users.json','w') as file:
-            json.dump(list,file)
-   
+        with open('users.json', 'w') as file:
+            json.dump(user_list, file)
+
     @CheckPath
     def loadusers(self):
-        with open("users.json",'r', encoding= 'utf-8') as file:
-            users = json.load(file)
-            for user in users:
-                new_user =  json.loads(user)
-                new_user = User(new_user['username'],new_user['password'],new_user['email'])
-                self.users.append(new_user)
+        if os.path.exists("users.json"):
+            with open("users.json", 'r', encoding='utf-8') as file:
+                users = json.load(file)
+                for user in users:
+                    new_user = json.loads(user)
+                    new_user = User(new_user['username'], new_user['password'], new_user['email'])
+                    self.users.append(new_user)
 
 repo = UserRepository()
 
 while True:
-    print(" Menü ".center(50,'*'))
-    secim = input("1- Register\n2- Login\n3- Logout\n4- Identity\n5- Exit\nSeçiminiz: ")
-    if secim == '1':
+    print(" Menu ".center(50, '*'))
+    choice = input("1- Register\n2- Login\n3- Logout\n4- Identity\n5- Exit\nYour choice: ")
+    
+    if choice == '1':
         username = input('Username: ')
-        password = input('password: ')
-        email = input('email: ')
-        user = User(username,password,email)
+        password = input('Password: ')
+        email = input('Email: ')
+        user = User(username, password, email)
         repo.Register(user)
     
-    elif secim == '2':
-        if repo.IsLoggedIn == True:
+    elif choice == '2':
+        if repo.IsLoggedIn:
             print('You are already logged in')
         else:
             username = input('Username: ')
-            password = input('password: ')
-            repo.Login(username,password)
+            password = input('Password: ')
+            repo.Login(username, password)
 
-    elif secim == '3':
-        if repo.IsLoggedIn == True:
+    elif choice == '3':
+        if repo.IsLoggedIn:
             repo.Logout()
         else:
             print('You are not already logged in')
-    elif secim == '4':
+    
+    elif choice == '4':
         repo.Identity()
-    else:
+    
+    elif choice == '5':
         print('EXITING ...')
         break
-
-
-
-
-
